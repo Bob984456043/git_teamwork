a01e26846db303b6dafa960fb42b2faa35761620
Bob <48949693+Bob984456043@users.noreply.github.com>
2019-07-12 16:01:22 +0800

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,5 +1,5 @@
 public abstract class Shape {
-    private double Perimeter;
+    private double perimeter;
     private double area;
     public abstract double getArea();
     public abstract double getPerimeter();
